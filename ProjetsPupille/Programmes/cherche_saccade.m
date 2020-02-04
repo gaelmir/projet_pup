d92aed6d2758e0ea643f101f49b774dce0accef9
gaelmir <miramondgael@gmail.com>
2020-02-04 09:12:09 +0100

    ajout decomposition en vecteurs de 100ms de l'image

--- ProjetsPupille/Programmes/cherche_saccade.m
+++
@@ -1,8 +0,0 @@
-clear all
-close all
-%%
-
-m=load('C:\Users\miram\Documents\IESE\IESE5\gitou\projet_pup\ProjetsPupille\Programmes\Exemple_Data\SCTf02.mat');
-gx=m.eyedata.FSAMPLE.gx;
-gy=m.eyedata.FSAMPLE.gy;
-
