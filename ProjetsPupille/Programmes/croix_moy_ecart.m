b856e0a1338369caad4aaf0634ef44f5adc8864c
gaelmir <miramondgael@gmail.com>
2020-02-04 11:32:37 +0100

    ajout dans la fct croix_moy_ecart de la liste des vecteurs conservés

--- ProjetsPupille/Programmes/croix_moy_ecart.m
+++ ProjetsPupille/Programmes/croix_moy_ecart.m
@@ -1,4 +1,4 @@
-function [moy_corr,ecart_corr,moy_err,ecart_err]=croix_moy_ecart(SubjectCode,path,face)
+function [garde_corr,moy_corr,ecart_corr,garde_err,moy_err,ecart_err]=croix_moy_ecart(SubjectCode,path,face)
 
 %input 
 %subject code : '02' entre ''
@@ -7,7 +7,7 @@ function [moy_corr,ecart_corr,moy_err,ecart_err]=croix_moy_ecart(SubjectCode,pat
 
 %output 
 %les statistiques sur les 400 dernières ms 
-
+%garde : numero des essais coservés
 
 if face ~= 0 && face ~=1
     print("erreur")
@@ -43,16 +43,19 @@ if face==0
 
     %Affichage de l'évolution en cas d'erreur pour sizePupilimage et
     %sizePupilfixationcross
-    tab_sizepupilimage_err_cross=[]
+    
+    tab_sizepupilimage_err_cross=[];
     moy_err=[];
     ecart_err=[];
+    garde_err=[];
     for i=1:size(Matrice_taille_erreur)
         sizepupilimage=cell2mat(Matrice_taille_erreur(i));
         sizepupilimage=sizepupilimage(end-399:end);
         if sum(sizepupilimage==0)==0 && sum(sizepupilimage>=8000)==0
+            garde_err=[garde_err;i];
             tab_sizepupilimage_err_cross=[tab_sizepupilimage_err_cross;sizepupilimage];
-            moy_err=[moy_err;mean(sizepupilimage)]
-            ecart_err=[ecart_err;std(sizepupilimage)]
+            moy_err=[moy_err;mean(sizepupilimage)];
+            ecart_err=[ecart_err;std(sizepupilimage)];
             x=size(sizepupilimage);
         end
     end
@@ -62,14 +65,16 @@ if face==0
     %sizePupilfixationcross
     moy_corr=[];
     ecart_corr=[];
+    garde_corr=[];
     tab_sizepupilimage_corr_cross=[]
     for i=1:size(Matrice_taille_correct)
         sizepupilimage=cell2mat(Matrice_taille_correct(i));
         sizepupilimage=sizepupilimage(end-399:end);
         if sum(sizepupilimage==0)==0 && sum(sizepupilimage>=8000)==0
+            garde_corr=[garde_corr;i];
             tab_sizepupilimage_corr_cross=[tab_sizepupilimage_corr_cross;sizepupilimage];
-            moy_corr=[moy_corr;mean(sizepupilimage)]
-            ecart_corr=[ecart_corr;std(sizepupilimage)]
+            moy_corr=[moy_corr;mean(sizepupilimage)];
+            ecart_corr=[ecart_corr;std(sizepupilimage)];
             x=size(sizepupilimage);
         end
     end
@@ -116,11 +121,13 @@ if face==1
     
     moy_err=[];
     ecart_err=[];
+    garde_err=[];
     tab_sizepupilimage_err_cross=[]
     for i=1:size(Matrice_taille_erreur)
         sizepupilimage=cell2mat(Matrice_taille_erreur(i));
         sizepupilimage=sizepupilimage(end-399:end);
         if sum(sizepupilimage==0)==0 && sum(sizepupilimage>=8000)==0
+            garde_err=[garde_err;i];
             tab_sizepupilimage_err_cross=[tab_sizepupilimage_err_cross;sizepupilimage];
             moy_err=[moy_err;mean(sizepupilimage)]
             ecart_err=[ecart_err;std(sizepupilimage)]
@@ -135,11 +142,13 @@ if face==1
     %sizePupilfixationcross
     moy_corr=[];
     ecart_corr=[];
+    garde_corr=[];
     tab_sizepupilimage_corr_cross=[]
     for i=1:size(Matrice_taille_correct)
         sizepupilimage=cell2mat(Matrice_taille_correct(i));
         sizepupilimage=sizepupilimage(end-399:end);
         if sum(sizepupilimage==0)==0 && sum(sizepupilimage>=8000)==0
+            garde_corr=[garde_corr;i];
             tab_sizepupilimage_corr_cross=[tab_sizepupilimage_corr_cross;sizepupilimage];
             moy_corr=[moy_corr;mean(sizepupilimage)]
             ecart_corr=[ecart_corr;std(sizepupilimage)]
